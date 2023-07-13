4dddeb7752827d31c5b79ad119123c6fc4c414ce
Thomas BUREAU DU COLOMBIER <sio.bureauducolombier@gmail.com>
2023-07-13 16:03:27 +0200

    Ajout de la page galerie

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -3,6 +3,7 @@ import Route from "./Route.js";
 //Définir ici vos routes
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
+    new Route("/galerie", "La galerie", "/pages/galerie.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
