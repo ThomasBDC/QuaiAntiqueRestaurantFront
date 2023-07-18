88889386bb567f8815602ebd87b3bc101055baac
Thomas BUREAU DU COLOMBIER <sio.bureauducolombier@gmail.com>
2023-07-18 16:59:03 +0200

    Ajout de la page réserver.html

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -9,6 +9,7 @@ export const allRoutes = [
     new Route("/account", "Mon compte", "/pages/auth/account.html"),
     new Route("/editPassword", "Changement de mot de passe", "/pages/auth/editPassword.html"),
     new Route("/allResa", "Vos réservations", "/pages/reservations/allResa.html"),
+    new Route("/reserver", "Réserver", "/pages/reservations/reserver.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
