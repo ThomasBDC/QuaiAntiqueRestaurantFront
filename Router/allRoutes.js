e1e96630e5c708aeacc3a2f9c982ca69b5b9a597
Thomas BUREAU DU COLOMBIER <sio.bureauducolombier@gmail.com>
2023-07-18 15:33:02 +0200

    Ajout de la page mon compte

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -4,7 +4,10 @@ import Route from "./Route.js";
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
     new Route("/galerie", "La galerie", "/pages/galerie.html"),
-    new Route("/signin", "Connexion", "/pages/signin.html"),
+    new Route("/signin", "Connexion", "/pages/auth/signin.html"),
+    new Route("/signup", "Inscription", "/pages/auth/signup.html"),
+    new Route("/account", "Mon compte", "/pages/auth/account.html"),
+    new Route("/editPassword", "Changement de mot de passe", "/pages/auth/editPassword.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
