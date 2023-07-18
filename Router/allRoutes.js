ad2e451fc3b87109da4ece313c3548697dc8ce30
Thomas BUREAU DU COLOMBIER <sio.bureauducolombier@gmail.com>
2023-07-18 15:03:56 +0200

    Page de connexion

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -4,6 +4,7 @@ import Route from "./Route.js";
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
     new Route("/galerie", "La galerie", "/pages/galerie.html"),
+    new Route("/signin", "Connexion", "/pages/signin.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
