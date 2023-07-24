64eec0e901a10fc59e966f8413a25bf55b5fa554
Thomas BUREAU DU COLOMBIER <sio.bureauducolombier@gmail.com>
2023-07-24 15:05:06 +0200

    Vérfication password

--- js/auth/signup.js
+++ js/auth/signup.js
@@ -18,12 +18,30 @@ function validateForm(){
     const nomOk = validateRequired(inputNom);
     const prenomOk = validateRequired(inputPreNom);
     const mailOk = validateMail(inputMail);
+    const passwordOk = validatePassword(inputPassword);
 
-    if(nomOk && prenomOk && mailOk){
+
+    if(nomOk && prenomOk && mailOk && passwordOk){
         btnValidation.disabled = false;
     }
     else{
         btnValidation.disabled = true;
+    }
+}
+
+function validatePassword(input){
+    //Définir mon regex
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_])[A-Za-z\d\W_]{8,}$/;
+    const passwordUser = input.value;
+    if(passwordUser.match(passwordRegex)){
+        input.classList.add("is-valid");
+        input.classList.remove("is-invalid"); 
+        return true;
+    }
+    else{
+        input.classList.remove("is-valid");
+        input.classList.add("is-invalid");
+        return false;
     }
 }
 
