a30483a730b23910a876744a3a997f9b3c0663b9
Thomas BUREAU DU COLOMBIER <sio.bureauducolombier@gmail.com>
2023-09-14 21:17:03 +0200

    Add function getInfosUser

--- js/script.js
+++ js/script.js
@@ -101,3 +101,30 @@ function sanitizeHtml(text){
     tempHtml.textContent = text;
     return tempHtml.innerHTML;
 }
+
+function getInfosUser(){
+    let myHeaders = new Headers();
+    myHeaders.append("X-AUTH-TOKEN", getToken());
+
+    let requestOptions = {
+        method: 'GET',
+        headers: myHeaders,
+        redirect: 'follow'
+    };
+
+    fetch(apiUrl+"account/me", requestOptions)
+    .then(response =>{
+        if(response.ok){
+            return response.json();
+        }
+        else{
+            console.log("Impossible de récupérer les informations utilisateur");
+        }
+    })
+    .then(result => {
+        return result;
+    })
+    .catch(error =>{
+        console.error("erreur lors de la récupération des données utilisateur", error);
+    });
+}
